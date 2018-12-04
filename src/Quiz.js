9319419daed440e55705babc4ded149e10778c9c
Scarlett Sidwell <scarlett.r.sidwell@accenture.com>
2018-12-04 11:03:36 -0600

    module4

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,17 +1,25 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import QuizQuestion from './QuizQuestion'
+import QuizEnd from './QuizEnd.js'
 
-let quizData = require('./quiz_data.json')
+let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {quiz_position: 1};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {quiz_position: 1};
+    }
 
-  render() {
-      return <div><QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/></div>
-  }
+    render() {
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
+        return (
+            <div>
+                {isQuizEnd ? <QuizEnd /> :
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
+            </div>
+        )
+
+    }
 }
 
 export default Quiz
