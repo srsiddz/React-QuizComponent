b829af8e649ef823fcebea9ed184dba570776047
Scarlett Sidwell <scarlett.r.sidwell@accenture.com>
2018-12-04 11:45:55 -0600

    module 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,6 +10,12 @@ class Quiz extends Component {
         this.state = {quiz_position: 1};
     }
 
+    handleResetClick() {
+        this.setState(() => {
+            return {quiz_position: 1};
+        });
+    }
+
     showNextQuestion() {
         this.setState((state) => {
             return {quiz_position: state.quiz_position + 1}
@@ -20,7 +26,7 @@ class Quiz extends Component {
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
         return (
             <div>
-                {isQuizEnd ? <QuizEnd /> :
+                {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> :
                 <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
             </div>
         )
